a041d502359a73c0c4851a22687987ada74fe519
bmichalczuk <barmic90@hotmail.com>
2022-01-11 21:59:01 +0100

    add react router

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,20 +1,43 @@
-import React, { useEffect, useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import './App.css';
-import useUser from "./hooks/useUser";
 import {loginWithGoogle, logOut} from "./firebase/utils";
+import Dashboard from "./components/Dashboard";
+import LandingPage from "./components/LandingPage";
+import {UserContext} from "./contexts/UserContext";
+import {Provider} from "react-redux";
+import store from "./redux/store";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
 
 function App() {
-  const user = useUser();
+
+  const user = useContext(UserContext);
+
 
   return (
     <div className="App">
+      <header>
       {!user 
-        ? <button onClick={loginWithGoogle}>login</button>
-        : <button onClick={logOut}>logout</button>
-      }
+          ? <button onClick={loginWithGoogle}>login</button>
+          : <button onClick={logOut}>logout</button>
+        }
+      </header>
       
+      <Provider store={store}>
+        <Router>
+          <Routes>
+            <Route  path="/" element={<LandingPage />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            
+           
+          </Routes>
+         
+
+          
+        </Router>
+        </Provider>
     </div>
   );
-}
+};
 
 export default App;
--- app/src/index.tsx
+++ app/src/index.tsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import AuthUserContext from "./contexts/UserContext";
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AuthUserContext>
+      <App />
+    </AuthUserContext>
   </React.StrictMode>,
   document.getElementById('root')
 );
